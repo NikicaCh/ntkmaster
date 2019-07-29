cdea848ad0fbfd20e3597bfd03f4f20e44ba69b2
unknown <nikica_chelsea@hotmail.com>
2019-07-29 09:44:21 +0200

    add to db working

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,14 +5,13 @@ import TextField from '@material-ui/core/TextField'
 import firebase from 'firebase'
 
 import moment from 'moment'
+import { tsThisType } from '@babel/types';
 
 
 
 class  MaterialTableDemo extends React.Component {
   constructor(props) {
     super(props);
-    console.log("data",this.props.records)
-
     this.state = {
       TableColumns: [],
       data: [],
@@ -21,74 +20,100 @@ class  MaterialTableDemo extends React.Component {
         textField: {
           // marginLeft: theme.spacing(1),
           // marginRight: theme.spacing(1),
-          
         }
       },
       newRecords: [],
-      working: false
+      updated: false,
+      updatingDatabase: false
     }
+    this.addToDb = this.addToDb.bind(this)
   }
 
   componentWillReceiveProps(nextProps) {
-    //state ako dodavas i ako ne dodavas
-    this.setState({data: nextProps.records})
-    Object.keys(nextProps.records[0]).map((key, index) => {
-      if(nextProps.records[0][key] === true) {
-        nextProps.records[0][key] = 
-          <Checkbox
-            checked={true}
-            color="primary"/>
-      } else if(nextProps.records[0][key] === false) {
-        nextProps.records[0][key] = 
-          <Checkbox
-            checked={false}
-            color="primary"/>
-      } else if(nextProps.records[0][key] === "") {
-        nextProps.records[0][key] = 
-        <TextField
-          id="standard-helperText"
-          // label="Insert Date"
-          defaultValue="Default Value"
-          className={this.state.classes.textField}
-          // helperText="default: today's date"
-          margin="normal"
-      />
-      }
-    })
-
+    if(! this.state.updated) {
+      this.setState({data: nextProps.records})
+      Object.keys(nextProps.records[0]).map((key, index) => {
+        if(nextProps.records[0][key] === true) {
+          nextProps.records[0][key] = 
+            <Checkbox
+              checked={true}
+              color="primary"/>
+        } else if(nextProps.records[0][key] === false) {
+          nextProps.records[0][key] = 
+            <Checkbox
+              checked={false}
+              color="primary"/>
+        } else if(nextProps.records[0][key] === "") {
+          nextProps.records[0][key] = 
+          <TextField
+            id="standard-helperText"
+            // label="Insert Date"
+            defaultValue="Default Value"
+            className={this.state.classes.textField}
+            // helperText="default: today's date"
+            margin="normal"
+        />
+        }
+        this.setState({data: nextProps.records})
+      })
+    }
+    this.setState({updated: true})
   }
 
   componentDidMount() {
-    console.log(moment("July 12, 2019 at 12:00:00 AM UTC+2").format())
+    
+    console.log(this.state.newRecords.length)
+    window.onbeforeunload = confirmExit;
+    function confirmExit() {
+      if(!this.state.newRecords.length == 0) {
+        return "You have attempted to leave this page. Are you sure?";
+      } else {
+        window.onbeforeunload = function () {
+          // blank function do nothing
+        }
+      }
+    }
+    
     const TableColumns = this.props.tableColumns;
     const style = {
       position: "absolute",
       top: "15%",
       width: "95%",
       left: "2.5%",
-      fontSize: "1vw"
+      fontSize: "1vw",
     }
     this.setState({TableColumns, style})
-    window.addEventListener("beforeunload", () => { 
-      this.addToDb(this.state.newRecords)
-    });
+    setInterval(() => {
+      this.addToDb(this.state.newRecords);
+    }, 30000)
   }
 
   addToDb = (data) => {
+    window.addEventListener("beforeunload", () => {
+      return "blah blah blah"
+    })
+    this.setState({updatingDatabase: true})
     data.map((_data) => {
       const uniqueId = _data.Bolla;
       firebase.firestore().collection("Bolla").doc(uniqueId).set(_data)
       .then(() => {
           console.log("Document successfully written!");
+          this.setState({newRecords: []})
+          window.addEventListener("beforeunload", () => {
+            //blank
+          })
       })
       .catch((error) => {
           console.error("Error writing document: ", error);
       });
     })
-    this.setState({newRecords: []})
+    this.setState({newRecords: [], updatingDatabase: false})
   }
   
   render() {
+    if(this.state.newRecords.length !== 0) {
+      this.addToDb(this.state.newRecords)
+    } 
     return (
       <MaterialTable
         title="Season 201"
@@ -107,7 +132,6 @@ class  MaterialTableDemo extends React.Component {
               newRecords.push(newData)
               this.setState({ ...this.state, data });
               this.setState({ ...this.state, newRecords });
-              this.setState({working: false})
             }, 600);
           }),
           onRowUpdate: (newData, oldData) =>
