8fd39cc331d76d8e3ab1bd91bd65347ad3f28f1e
Nikica Maksimovski <nikica_chelsea@hotmail.com>
2020-03-03 13:10:40 +0100

    new app, new season

--- src/index.js
+++ src/index.js
@@ -10,15 +10,14 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyAMElcbneox6gS4P74lwtLjWpOcxBLMq_w",
-    authDomain: "ntkmaster-4b600.firebaseapp.com",
-    databaseURL: "https://ntkmaster-4b600.firebaseio.com",
-    projectId: "ntkmaster-4b600",
-    storageBucket: "ntkmaster-4b600.appspot.com",
-    messagingSenderId: "506995767744",
-    appId: "1:506995767744:web:615eaacf826888d0"
-});
+const app = firebase.initializeApp({ 
+    apiKey: "AIzaSyDL3zNBxGXra7SuJ3dUmbzhhlF5cX5t014",
+    authDomain: "ntkmaster202.firebaseapp.com",
+    databaseURL: "https://ntkmaster202.firebaseio.com",
+    projectId: "ntkmaster202",
+    storageBucket: "ntkmaster202.appspot.com",
+    messagingSenderId: "593618568565",
+    appId: "1:593618568565:web:8d40d1d89da1a5d730bf3c" });
 const db = firebase.firestore(app);
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -26,4 +25,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
